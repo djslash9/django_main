f069a0060e206d5b8def13b19fd32c512699e689
djslash9 <djslash@ymail.com>
2025-03-05 10:13:36 +0530

    modified:   crm/crm/settings.py 	modified:   crm/crm/urls.py 	new file:   crm/templates/base.html 	new file:   crm/templates/home.html 	new file:   crm/templates/navbar.html 	new file:   crm/webapp/urls.py 	modified:   crm/webapp/views.py

--- crm/crm/settings.py
+++ crm/crm/settings.py
@@ -14,7 +14,8 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+TEMPLATES_DIR = BASE_DIR / 'templates'
+STATIC_DIR = BASE_DIR / 'static'
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'crm.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATES_DIR,],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
