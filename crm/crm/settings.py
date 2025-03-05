07c58b5c970b4c8989b2529515eca9b65dbc17d2
djslash9 <djslash@ymail.com>
2025-03-05 08:57:34 +0530

    new file:   .vscode/settings.json modified:   crm/crm/settings.py

--- crm/crm/settings.py
+++ crm/crm/settings.py
@@ -73,12 +73,24 @@ WSGI_APPLICATION = 'crm.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+#}
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'crmdb',
+        "USER": 'root',
+        "PASSWORD": 'Spd@2025',
+        "HOST": 'localhost',
+        "PORT": '3306',
     }
 }
+
 
 
 # Password validation
