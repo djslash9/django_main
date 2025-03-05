e407670f91552bf2255524e122cd682669967f3b
djslash9 <djslash@ymail.com>
2025-03-05 14:46:46 +0530

    new file:   DjangoDB.session.sql 	modified:   crm/webapp/models.py

--- crm/webapp/models.py
+++ crm/webapp/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class Client(models.Model):
+    full_name = models.CharField(max_length=100)
+    email = models.EmailField(max_length=50)
+    phone = models.CharField(max_length=15)
+    address = models.TextField(max_length=250)
+    city = models.CharField(max_length=100)
+    zip_code = models.CharField(max_length=100)
+    created-at = models.DateTimeField(auto_now_add=True)
+    
+
+    def __str__(self):
+        return (f"{self.full_name} {self.email}")
+    
